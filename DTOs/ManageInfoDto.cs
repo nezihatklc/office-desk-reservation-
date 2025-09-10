2a0c191c3ced7d64911163605ab7b37fce59416b
Ahmet Furkan Demir <furkandemr2001@gmail.com>
2025-09-10 09:30:53 +0300

    - deleted not used dto file ManageInfoDto.cs

--- DTOs/ManageInfoDto.cs
+++
@@ -1,8 +0,0 @@
-namespace AuthDemo.DTOs;
-
-public class ManageInfoDto
-{
-    public int UserId { get; set; }
-    public string? FirstName { get; set; }
-    public string? LastName { get; set; }
-}
