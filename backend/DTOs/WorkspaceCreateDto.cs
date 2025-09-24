6d73fb535ce3aafa066440c1fde3d4cf45766c7a
Ahmet Furkan Demir <furkandemr2001@gmail.com>
2025-09-24 12:03:40 +0300

    Added TeamName variable for workspace name pinned teams

--- backend/DTOs/WorkspaceCreateDto.cs
+++ backend/DTOs/WorkspaceCreateDto.cs
@@ -5,7 +5,7 @@ namespace backend.DTOs
         public string WorkspaceName { get; set; } = null!;
         public string FloorNumber { get; set; } = null!;
         public string DeskCode { get; set; } = null!;
-        
         public int Capacity { get; set; }
+        public string? TeamName { get; set; }
     }
 }
