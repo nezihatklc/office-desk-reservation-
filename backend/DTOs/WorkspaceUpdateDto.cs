d269bb2960f2b546eaf9535a6b29b307d07dbaa4
Ahmet Furkan Demir <furkandemr2001@gmail.com>
2025-09-24 12:07:09 +0300

    Added workspaceupdatedto file for updating workspace information

--- backend/DTOs/WorkspaceUpdateDto.cs
+++ backend/DTOs/WorkspaceUpdateDto.cs
@@ -1,9 +1,11 @@
 namespace backend.DTOs
 {
-    public class WorkspaceUpdateDto
-    {
-        public string WorkspaceName { get; set; } = null!;
-        public string FloorNumber { get; set; } = null!;
-        public string DeskCode { get; set; } = null!;
-    }
+public class WorkspaceUpdateDto
+{
+    public string? WorkspaceName { get; set; }
+    public string? FloorNumber { get; set; }
+    public string? DeskCode { get; set; }
+    public int? Capacity { get; set; }
+    public string? TeamName { get; set; }
+}
 }
