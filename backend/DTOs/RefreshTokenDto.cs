05c9cf33763f992ab4b5346bbc2f211499ef9a0f
Ahmet Furkan Demir <furkandemr2001@gmail.com>
2025-09-24 11:53:41 +0300

    Deleted unused dto file from system

--- backend/DTOs/RefreshTokenDto.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace backend.DTOs;
-
-public class RefreshTokenDto
-{
-    [Required(ErrorMessage = "Refresh token is required.")]
-    public string RefreshToken { get; set; }
-}
