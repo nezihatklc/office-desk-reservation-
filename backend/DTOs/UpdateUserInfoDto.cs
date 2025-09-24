b8224a17e37c1be8b009c1b5eeb07262ee717367
Ahmet Furkan Demir <furkandemr2001@gmail.com>
2025-09-24 11:08:01 +0300

    Add update user info DTO

---
+++ backend/DTOs/UpdateUserInfoDto.cs
@@ -0,0 +1,8 @@
+namespace backend.DTOs;
+
+public class UpdateUserInfoDto
+{
+    public int UserId { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+}
